39f4cff72bda7bab98afaed80c3620e57a12aab7
Nico Schlömer <nico.schloemer@gmail.com>
2017-01-30 21:24:28 +0100

    fewer dependencies

--- setup.py
+++ setup.py
@@ -37,9 +37,7 @@ setup(
     install_requires=[
         'meshio',
         'numpy',
-        'scipy',
-        'sympy',
-        'vtk'
+        'scipy'
         ],
     classifiers=[
         'Development Status :: 4 - Beta',
