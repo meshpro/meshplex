816a91b13c18b7cb3256675d0d1c7dc8eda62312
Nico Schlömer <nico.schloemer@gmail.com>
2017-02-01 11:51:52 +0100

    add python 3 tests

--- setup.py
+++ setup.py
@@ -47,6 +47,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Topic :: Scientific/Engineering :: Mathematics'
         ]
     )
