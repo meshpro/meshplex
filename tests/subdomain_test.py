0c6da65755860d58359dd1d849c47ffddab2f09c
Nico Schlömer <nico.schloemer@gmail.com>
2017-05-19 00:16:02 +0200

    better subdomain test

--- tests/subdomain_test.py
+++ tests/subdomain_test.py
@@ -25,15 +25,23 @@ def test_mark_subdomain():
     mesh, _, _, _ = voropy.read(filename)
     # mesh.mark_default_subdomains()
 
-    class Subdomain(object):
-        def __init__(self):
-            self.is_boundary_only = True
-            return
+    class Subdomain1(object):
+        is_boundary_only = True
 
         def is_inside(self, x):
             return x[0] < 0.0
 
-    sd = Subdomain()
-    mesh.mark_vertices(sd)
-    assert len(mesh.subdomains[sd]['vertices']) == 27
+    class Subdomain2(object):
+        is_boundary_only = False
+
+        def is_inside(self, x):
+            return x[0] > 0.0
+
+    sd1 = Subdomain1()
+    mesh.mark_vertices(sd1)
+    assert len(mesh.subdomains[sd1]['vertices']) == 27
+
+    sd2 = Subdomain2()
+    mesh.mark_vertices(sd2)
+    assert len(mesh.subdomains[sd2]['vertices']) == 214
     return
