4471446549fa38b32bbd8a8e6189c8596713d37b
Nico Schlömer <nico.schloemer@gmail.com>
2020-11-13 18:00:39 +0100

    test set_points

--- test/test_mesh_tri.py
+++ test/test_mesh_tri.py
@@ -12,7 +12,11 @@ import meshplex
 this_dir = pathlib.Path(__file__).resolve().parent
 
 
-@pytest.mark.parametrize("cells_dtype", [numpy.int64, numpy.uint64])
+@pytest.mark.parametrize("cells_dtype", [
+    numpy.int64
+    # depends on <https://github.com/numpy/numpy/issues/17760>
+    # numpy.uint64
+])
 def test_unit_triangle(cells_dtype):
     points = numpy.array([[0.0, 0.0], [1.0, 0.0], [0.0, 1.0]])
     cells = numpy.array([[0, 1, 2]], dtype=cells_dtype)
@@ -888,6 +892,18 @@ def test_remove_cells(remove_idx, expected_num_cells, expected_num_edges):
     mesh.remove_cells(remove_idx)
     assert len(mesh.cells["points"]) == expected_num_cells
     assert len(mesh.edges["points"]) == expected_num_edges
+
+
+def test_set_points():
+    points = numpy.array([[0.0, 0.0], [1.0, 0.0], [0.0, 1.0]])
+    cells = numpy.array([[0, 1, 2]])
+    mesh = meshplex.MeshTri(points, cells)
+
+    mesh.set_points([0.1, 0.1], [0])
+    ref = mesh.cell_volumes.copy()
+
+    mesh2 = meshplex.MeshTri(mesh.points, mesh.cells["points"])
+    assert numpy.all(numpy.abs(ref - mesh2.cell_volumes)) < 1.0e-10
 
 
 if __name__ == "__main__":
