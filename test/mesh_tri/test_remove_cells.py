6d94a658391834929c514281c4ddeb6211fefd71
Nico Schlömer <nico.schloemer@gmail.com>
2020-11-20 10:33:55 +0100

    more tests for remove_cells

--- test/mesh_tri/test_remove_cells.py
+++ test/mesh_tri/test_remove_cells.py
@@ -8,7 +8,15 @@ import meshplex
 this_dir = pathlib.Path(__file__).resolve().parent
 
 
-def _get_test_mesh():
+def get_mesh0():
+    # _____________
+    # |   _/ \_   |
+    # | _/     \_ |
+    # |/_________\|
+    # |\_       _/|
+    # |  \_   _/  |
+    # |___ \_/____|
+    #
     points = numpy.array(
         [
             [0.0, 0.0],
@@ -36,6 +44,25 @@ def _get_test_mesh():
     return mesh
 
 
+def get_mesh1():
+    # _____________
+    # |\_       _/|
+    # |  \_   _/  |
+    # |    \_/    |
+    # |   _/ \_   |
+    # | _/     \_ |
+    # |/_________\|
+    #
+    points = [[0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0], [0.5, 0.5]]
+    cells = [[0, 1, 4], [1, 2, 4], [2, 3, 4], [0, 4, 3]]
+    return meshplex.MeshTri(points, cells)
+
+
+def get_mesh2():
+    mesh0 = meshplex.read(this_dir / ".." / "meshes" / "pacman.vtk")
+    return meshplex.MeshTri(mesh0.points[:, :2], mesh0.cells["points"])
+
+
 @pytest.mark.parametrize(
     "remove_idx,expected_num_cells,expected_num_edges",
     [
@@ -48,7 +75,7 @@ def _get_test_mesh():
     ],
 )
 def test_remove_cells(remove_idx, expected_num_cells, expected_num_edges):
-    mesh = _get_test_mesh()
+    mesh = get_mesh0()
     assert len(mesh.cells["points"]) == 6
     assert len(mesh.edges["points"]) == 13
     # remove a corner cell
@@ -58,9 +85,7 @@ def test_remove_cells(remove_idx, expected_num_cells, expected_num_edges):
 
 
 def test_remove_cells_boundary():
-    points = [[0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0], [0.5, 0.5]]
-    cells = [[0, 1, 4], [1, 2, 4], [2, 3, 4], [0, 4, 3]]
-    mesh = meshplex.MeshTri(points, cells)
+    mesh = get_mesh1()
 
     assert numpy.all(mesh.is_boundary_point == [True, True, True, True, False])
     assert numpy.all(mesh.is_boundary_edge_local[0] == [False, False, False, False])
@@ -92,9 +117,15 @@ def test_remove_all():
     assert not numpy.any(mesh.is_point_used)
 
 
-def test_reference():
-    mesh0 = meshplex.read(this_dir / ".." / "meshes" / "pacman.vtk")
-    mesh0 = meshplex.MeshTri(mesh0.points[:, :2], mesh0.cells["points"])
+@pytest.mark.parametrize(
+    "mesh0, remove_cells",
+    [
+        # (get_mesh0(), [0]),
+        # (get_mesh1(), [1, 2]),
+        (get_mesh2(), [0, 3, 57, 59, 60, 61, 100]),
+    ],
+)
+def test_reference(mesh0, remove_cells):
     # some dummy calls to make sure the respective value are computed before the cell
     # removal and then updated
     mesh0.is_boundary_point
@@ -106,7 +137,7 @@ def test_reference():
     mesh0.signed_cell_areas
     mesh0.cell_centroids
     # now remove some cells
-    mesh0.remove_cells([0, 3, 57, 59, 60, 61, 100])
+    mesh0.remove_cells(remove_cells)
 
     # recreate the reduced mesh from scratch
     mesh1 = meshplex.MeshTri(mesh0.points, mesh0.cells["points"])
