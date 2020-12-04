c55c530477d01d0dcbeff747b774d303f8f867d3
Nico Schlömer <nico.schloemer@gmail.com>
2020-12-04 13:07:00 +0100

    test more mesh equality

--- test/mesh_tri/test_remove_cells.py
+++ test/mesh_tri/test_remove_cells.py
@@ -10,6 +10,7 @@ this_dir = pathlib.Path(__file__).resolve().parent
 
 def compute_all_entities(mesh):
     mesh.is_boundary_point
+    mesh.is_interior_point
     mesh.is_boundary_edge_local
     mesh.is_boundary_edge
     mesh.is_boundary_cell
@@ -17,7 +18,42 @@ def compute_all_entities(mesh):
     mesh.ce_ratios
     mesh.signed_cell_areas
     mesh.cell_centroids
+    mesh.control_volumes
     mesh.create_edges()
+    mesh.edges_cells
+    mesh.edges_cells_idx
+    mesh.boundary_edges
+    mesh.interior_edges
+    mesh.cell_circumcenters
+    mesh.ce_ratios_per_interior_edge
+    mesh.control_volume_centroids
+
+    assert mesh._cv_cell_mask is not None
+    assert mesh.edges is not None
+    assert mesh.subdomains is not {}
+    assert mesh._is_interior_point is not None
+    assert mesh._is_boundary_point is not None
+    assert mesh._is_boundary_edge_local is not None
+    assert mesh._is_boundary_edge is not None
+    assert mesh._is_boundary_cell is not None
+    assert mesh._edges_cells is not None
+    assert mesh._edges_cells_idx is not None
+    assert mesh._boundary_edges is not None
+    assert mesh._interior_edges is not None
+    assert mesh._is_point_used is not None
+    assert mesh._half_edge_coords is not None
+    assert mesh._ei_dot_ei is not None
+    assert mesh._ei_dot_ej is not None
+    assert mesh._cell_volumes is not None
+    assert mesh._ce_ratios is not None
+    assert mesh._cell_circumcenters is not None
+    assert mesh._interior_ce_ratios is not None
+    assert mesh._control_volumes is not None
+    assert mesh._cell_partitions is not None
+    assert mesh._cv_centroids is not None
+    assert mesh._cvc_cell_mask is not None
+    assert mesh._signed_cell_areas is not None
+    assert mesh._cell_centroids is not None
 
 
 def assert_mesh_equality(mesh0, mesh1):
@@ -27,17 +63,39 @@ def assert_mesh_equality(mesh0, mesh1):
     assert numpy.all(numpy.abs(mesh0.points - mesh1.points) < 1.0e-14)
     assert numpy.all(mesh0.edges["points"] == mesh1.edges["points"])
 
+    assert numpy.all(mesh0.edges_cells[0] == mesh1.edges_cells[0])
+    assert numpy.all(mesh0.edges_cells[1] == mesh1.edges_cells[1])
+    assert numpy.all(mesh0.edges_cells_idx == mesh1.edges_cells_idx)
+
+    assert numpy.all(mesh0.boundary_edges == mesh1.boundary_edges)
+    assert numpy.all(mesh0.interior_edges == mesh1.interior_edges)
+
     assert numpy.all(mesh0.is_boundary_point == mesh1.is_boundary_point)
+    assert numpy.all(mesh0.is_interior_point == mesh1.is_interior_point)
     assert numpy.all(mesh0.is_boundary_edge_local == mesh1.is_boundary_edge_local)
     assert numpy.all(mesh0.is_boundary_edge == mesh1.is_boundary_edge)
     assert numpy.all(mesh0.is_boundary_cell == mesh1.is_boundary_cell)
 
+    assert numpy.all(numpy.abs(mesh0.ei_dot_ei - mesh1.ei_dot_ei) < 1.0e-14)
+    assert numpy.all(numpy.abs(mesh0.ei_dot_ej - mesh1.ei_dot_ej) < 1.0e-14)
     assert numpy.all(numpy.abs(mesh0.cell_volumes - mesh1.cell_volumes) < 1.0e-14)
     assert numpy.all(numpy.abs(mesh0.ce_ratios - mesh1.ce_ratios) < 1.0e-14)
+    assert numpy.all(
+        numpy.abs(mesh0.ce_ratios_per_interior_edge - mesh1.ce_ratios_per_interior_edge)
+        < 1.0e-14
+    )
     assert numpy.all(
         numpy.abs(mesh0.signed_cell_areas - mesh1.signed_cell_areas) < 1.0e-14
     )
     assert numpy.all(numpy.abs(mesh0.cell_centroids - mesh1.cell_centroids) < 1.0e-14)
+    assert numpy.all(
+        numpy.abs(mesh0.cell_circulcenters - mesh1.cell_circumcenters) < 1.0e-14
+    )
+    assert numpy.all(numpy.abs(mesh0.control_volumes - mesh1.control_volumes) < 1.0e-14)
+    assert numpy.all(
+        numpy.abs(mesh0.control_volume_centroids - mesh1.control_volume_centroids)
+        < 1.0e-14
+    )
 
 
 def get_mesh0():
