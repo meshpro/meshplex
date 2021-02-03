8ebd80b7e408f950a2d9cf4b9e7e2d4c2e2fd508
Nico Schlömer <nico.schloemer@gmail.com>
2021-02-03 18:00:28 +0100

    add test for point removal

---
+++ test/mesh_tri/test_remove_points.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+import meshplex
+
+from .helpers import assert_mesh_consistency
+
+
+def test():
+    points = [
+        [-2.1, -3.1],
+        [0.0, 0.0],
+        [1.0, 0.0],
+        [-2.1, -3.1],
+        [1.0, 1.0],
+        [0.0, 1.0],
+    ]
+    cells = [[1, 2, 4], [1, 4, 5]]
+    mesh = meshplex.MeshTri(points, cells)
+    mesh.control_volumes
+    mesh.control_volume_centroids
+    mesh.create_edges()
+
+    mesh.remove_dangling_points()
+
+    assert len(mesh.points) == 4
+    assert np.all(mesh.cells["points"] == np.array([[0, 1, 2], [0, 2, 3]]))
+    assert np.all(
+        mesh.edges["points"] == np.array([[0, 1], [0, 2], [0, 3], [1, 2], [2, 3]])
+    )
+    assert_mesh_consistency(mesh)
